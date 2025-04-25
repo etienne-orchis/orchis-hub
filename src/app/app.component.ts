72e44f3f6998aeda85f6bf6658cf470e0a5f75e0
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-04-25 14:19:04 +0300

    setting up basic interface on for the employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { MyDbService } from './services/my-db.service';
+import { Employee } from './interfaces/employee';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'orchis-hub';
+  dbService = inject(MyDbService);
+  employees: Employee[] | null = null;
+
+  employeeForm = new FormGroup({
+    name: new FormControl(''),
+  });
+
+  ngOnInit() {
+    console.log('Before fetch', this.employees);
+    (async () => {
+      this.employees = await this.dbService.getAllEmployees();
+      console.log('After fetch', this.employees);
+    })();
+  }
+
+  async addEmployee() {
+    const name = this.employeeForm.value.name as string;
+    const _id = faker.string.uuid();
+    await this.dbService.addEmployee({ _id, name });
+    this.employeeForm.reset();
+    this.employees = null;
+    const newEmployees = await this.dbService.getAllEmployees();
+    this.employees = newEmployees;
+  }
 }
